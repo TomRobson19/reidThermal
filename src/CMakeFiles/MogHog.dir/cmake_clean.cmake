2adf88ed1c8a92bde5bb998f98852642c5efc8a5
TomRobson19 <tomrobson19@gmail.com>
2016-10-26 16:56:56 +0100

    Merged Mog and Hog, added new CMakeLists file in src for testing

---
+++ src/CMakeFiles/MogHog.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MogHog.dir/mog_and_hog.cpp.o"
+  "MogHog.pdb"
+  "MogHog"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MogHog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
